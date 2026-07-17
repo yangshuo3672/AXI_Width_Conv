be855d0d5aebaac1a4a5897854151b0de1b43e3a
yangshuo3672 <xys5793@gmail.com>
2026-07-17 14:36:45 +0800

    Update ktp_skid_buffer.sv

--- ktp_skid_buffer.sv
+++ ktp_skid_buffer.sv
@@ -20,13 +20,13 @@ module ktp_skid_buffer #(
 //two stage register:
 
 //bypass(entry 0):
-    logic             out_valid;  //direct wire port valid_o
-    logic [WIDTH-1:0]  out_data;  //port data_o
+    logic              out_valid;    //direct wire port valid_o
+    logic [WIDTH-1:0]  out_data;     //port data_o
 //skid buffer(entry 1):
-    logic             skid_valid; //buffer valid
+    logic              skid_valid; //buffer valid
     logic [WIDTH-1:0]  skid_data; //buffer data
 
-    wire   input_fire    = valid_i && ready_o;  //upstream  handshake
+    wire   input_fire   = valid_i && ready_o;  //upstream  handshake
     wire   output_fire  = valid_o && ready_i;  //downstream handshake
 
     //wire  output_empty = !out_valid;
@@ -41,13 +41,46 @@ module ktp_skid_buffer #(
 
 //when need to pop data,if skid buffer has data,output skid's data.if skid buffer is empty,so look upstream whether has new data(input_fire).
 //
-    always_ff@ (posedge clk or negedge resetn)begin
-        if(!resetn)begin
-            out_valid  <= 1'b0;
-            out_data   <= 'd0;
+    always_ff@ (posedge clk or negedge resetn) begin
+    if(!resetn) begin
+        out_valid <= 1'b0;
+        out_data  <= 'd0;
+    end
+    else begin
+        if(output_fire) begin   // 下游能接收
+            if(skid_valid) begin // Skid Buffer 有数据 → 优先从缓冲区取
+                out_data  <= skid_data;
+                out_valid <= 1'b1;
+            end
+            else begin          // 否则直接传输入数据
+                out_valid <= input_fire;
+                if(input_fire) begin
+                    out_data <= data_i;
+                end
+            end
         end
-        else begin
-            if(output_fire)begin
-                if(skid_valid)begin
-                    out_data  <= skid_data;
-                    out_valid <= 1'b1;
+        else if(input_fire && !out_valid) begin // 下游不能收，但上游发了且当前无输出
+            out_data <= data_i;
+            out_valid <= 1'b1;
+        end
+    end
+end
+
+  always_ff @ (posedge clk or negedge resetn) begin
+    if(!resetn) begin
+        skid_valid <= 1'b0;
+        skid_data  <= 'd0;
+    end
+    else begin
+        if(output_fire) begin       // 下游能接收 → 清空缓冲区
+            skid_valid <= 1'b0;
+        end
+        if(input_fire && out_valid && !ready_i) begin // 上游发 + 当前有输出 + 下游不能收 → 存入缓冲区
+            skid_valid <= 1'b1;
+            skid_data  <= data_i;
+        end
+    end
+  end
+
+
+endmodule
