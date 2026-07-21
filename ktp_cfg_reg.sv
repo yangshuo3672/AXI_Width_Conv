33813dcbd4d99aee111d9f5b25907e042895c131
yangshuo3672 <xys5793@gmail.com>
2026-07-21 09:24:42 +0800

    Update ktp_cfg_reg.sv

--- ktp_cfg_reg.sv
+++ ktp_cfg_reg.sv
@@ -55,10 +55,7 @@ module ktp_cfg_reg #(
   localparam logic [11:0] REG_DBG_2     = 12'h108;
   localparam logic [11:0] REG_DBG_3     = 12'h10c;
 
-  logic [TRACK_ID_COUNT-1:0]      aw_valid_q;
-  logic [TRACK_ID_COUNT-1:0]      ar_valid_q;
-  logic [ADDR_WIDTH-1:0]          aw_addr_q [TRACK_ID_COUNT];
-  logic [ADDR_WIDTH-1:0]          ar_addr_q [TRACK_ID_COUNT];
+  localparam ADDR_ID_BUFFER_DEPTH = 16;
 
   logic [1:0]                     irpt_msk_q;
   logic [1:0]                     irpt_raw_q;
@@ -68,15 +65,13 @@ module ktp_cfg_reg #(
   logic [ID_WIDTH-1:0]            err_wr_id_q;
 
   logic                           apb_access;
-  logic                           apb_write;
-  logic                           valid_write_strobe;
+  logic                           apb_valid_write;
   logic [1:0]                     irpt_clr_pulse;
   logic                           r_error_fire;
   logic                           b_error_fire;
 
   assign apb_access         = psel && penable;
-  assign apb_write          = apb_access && pwrite;
-  assign valid_write_strobe = (pstrb == 4'b1111);
+  assign apb_valid_write    = apb_access && pwrite &&  (pstrb == 4'b1111)；
   assign pready             = 1'b1;
   assign pslverr            = 1'b0;
 
@@ -85,6 +80,23 @@ module ktp_cfg_reg #(
   assign ktp_irpt_ns  = |(irpt_raw_q & ~irpt_msk_q);
   assign irpt_clr_pulse = (apb_write && valid_write_strobe && (paddr == REG_IRPT_CLR)) ? pwdata[1:0] : 2'b00;
 
+
+  always_ff@(posedge clk or negedge resetn)begin
+    if(!resetn)begin
+      ktp_irpt_ns <= 1'b0;
+    end
+    else if(  |(irpt_raw_q & ~irpt_msk_q) )begin
+      ktp_irpt_ns <= 1'b1;
+    end
+    else begin
+      ktp_irpt_ns <= 1'b0;
+    end
+  end
+
+  logic [ID_WIDTH+ADDR_WIDTH+1-1 : 0 ] aw_addr_id_table [ADDR_ID_BUFFER_DEPTH];
+  logic [ID_WIDTH+ADDR_WIDTH+1-1 : 0 ] ar_addr_id_table [ADDR_ID_BUFFER_DEPTH];
+
+  
   always_ff @(posedge clk or negedge resetn) begin
     if (!resetn) begin
       ckg_bypass   <= 1'b0;
@@ -94,14 +106,12 @@ module ktp_cfg_reg #(
       err_wr_addr_q <= '0;
       err_rd_id_q   <= '0;
       err_wr_id_q   <= '0;
-      aw_valid_q    <= '0;
-      ar_valid_q    <= '0;
-      for (int i = 0; i < TRACK_ID_COUNT; i++) begin
-        aw_addr_q[i] <= '0;
-        ar_addr_q[i] <= '0;
+      for (int i = 0; i < ADDR_ID_BUFFER_DEPTH; i++) begin
+        aw_addr_id_table[i] <= '0;
+        ar_addr_id_table[i] <= '0;
       end
     end else begin
-      if (apb_write && valid_write_strobe) begin
+      if (apb_valid_write) begin
         unique case (paddr)
           REG_GLB_CTRL: ckg_bypass <= pwdata[0];
           REG_IRPT_MSK: irpt_msk_q <= pwdata[1:0];
@@ -109,50 +119,92 @@ module ktp_cfg_reg #(
           end
         endcase
       end
+      
+//restore the every beat cmd addr/id in invalid table and set the table valid
+   if(aw_fire)begin
+       for(int aw_index = 0;aw_index < ADDR_ID_BUFFER_DEPTH;aw_index++)begin
+           if( aw_addr_id_table[aw_index][40] == 0)begin   //valid
+               aw_addr_id_table[aw_index]     <= {1'b1,aw_id,aw_addr};
+               break;                                        //only detect one free buffer,after push id/addr to buffer,set buffer valid:bit[40]<=1'b1;
+           end
+       end
+   end
 
-      if (aw_fire) begin
-        aw_valid_q[aw_id] <= 1'b1;
-        aw_addr_q[aw_id]  <= aw_addr;
+  if(ar_fire)begin
+      for(int ar_index = 0;ar_index < ADDR_ID_BUFFER_DEPTH;ar_index++)begin
+          if( ar_addr_id_table[ar_index][40] == 0)begin
+              ar_addr_id_table[ar_index]     <= {1'b1,ar_id,ar_addr};
+              break;
+          end
       end
-      if (ar_fire) begin
-        ar_valid_q[ar_id] <= 1'b1;
-        ar_addr_q[ar_id]  <= ar_addr;
-      end
-
-      if (b_error_fire && !irpt_raw_q[1]) begin
-        err_wr_addr_q <= aw_addr_q[b_id];
-        err_wr_id_q   <= b_id;
-      end
-      if (r_error_fire && !irpt_raw_q[0]) begin
-        err_rd_addr_q <= ar_addr_q[r_id];
-        err_rd_id_q   <= r_id;
-      end
-
-      irpt_raw_q[1] <= (irpt_raw_q[1] && !irpt_clr_pulse[1]) || b_error_fire;
-      irpt_raw_q[0] <= (irpt_raw_q[0] && !irpt_clr_pulse[0]) || r_error_fire;
-
-      if (b_fire) begin
-        aw_valid_q[b_id] <= 1'b0;
-      end
-      if (r_fire && r_last) begin
-        ar_valid_q[r_id] <= 1'b0;
-      end
-    end
   end
 
-  always_comb begin
-    unique case (paddr)
-      REG_GLB_CTRL:  prdata = {31'b0, ckg_bypass};
-      REG_IRPT_MSK:  prdata = {30'b0, irpt_msk_q};
-      REG_IRPT_RAW:  prdata = {30'b0, irpt_raw_q};
-      REG_IRPT_STAT: prdata = {30'b0, (irpt_raw_q & ~irpt_msk_q)};
-      REG_IRPT_CLR:  prdata = 32'b0;
-      REG_DBG_0:     prdata = err_rd_addr_q;
-      REG_DBG_1:     prdata = err_wr_addr_q;
-      REG_DBG_2:     prdata = {16'b0, err_wr_id_q, err_rd_id_q};
-      REG_DBG_3:     prdata = dbg_info_3;
-      default:       prdata = 32'b0;
-    endcase
+  //when error response;look up the addr/id in the valid table and set the table invalid
+  if (b_error_fire && !irpt_raw_q[1]) begin          //if b channel return error && current no write error interrupt
+      for(int aw_index=0;aw_index < ADDR_ID_BUFFER_DEPTH;aw_index++)begin
+          if(( b_id == aw_addr_id_table[aw_index][39:32] ) && ( aw_addr_id_table[aw_index][40] ==1'b1 ))begin
+              err_wr_addr_q     <= aw_addr_id_table[aw_index][31:0];
+              err_wr_id_q       <= b_id;
+              aw_addr_id_table[aw_index][40] <= 1'b0;   //set invalid
+          end
+      end
+  end
+
+  if (r_error_fire && !irpt_raw_q[0]) begin          //if r channel return error && current no read error interrupt
+      for(int ar_index=0;ar_index < ADDR_ID_BUFFER_DEPTH;ar_index++)begin
+          if(( r_id == ar_addr_id_table[ar_index][39:32] ) && ( ar_addr_id_table[ar_index][40]==1'b1 ))begin
+              err_rd_addr_q     <= ar_addr_id_table[ar_index][31:0];
+              err_rd_id_q       <= r_id;
+              ar_addr_id_table[ar_index][40] <= 1'b0;
+          end
+      end
+  end
+      
+//when correct response: also set the table invalid
+  if( b_fire )begin
+      for(int i=0; i < ADDR_ID_BUFFER_DEPTH;i++)begin
+          if( ( b_id == aw_addr_id_table[i][39:32] ) && ( aw_addr_id_table[i][40] == 1'b1 ) )begin
+              aw_addr_id_table[i][40] <= 1'b0;
+          end
+      end
+  end
+
+  if( r_fire && r_last )begin
+      for(int i=0; i < ADDR_ID_BUFFER_DEPTH;i++)begin
+          if( ( r_id == ar_addr_id_table[i][39:32] ) && ( ar_addr_id_table[i][40] == 1'b1 ) )begin
+              ar_addr_id_table[i][40] <= 1'b0;
+          end
+      end
+  end
+
+   irpt_raw_q[1] <= (irpt_raw_q[1] && !irpt_clr_pulse[1]) || b_error_fire; //clear old error interrupt or generate new error interrupt
+   irpt_raw_q[0] <= (irpt_raw_q[0] && !irpt_clr_pulse[0]) || r_error_fire;
+      
+ end
+end
+  
+  always_ff@(posedge clk or negedge resetn) begin
+    if(!resetn)begin
+      prdata <= 'd0;
+    end
+    else if(psel && !pwrite)begin
+      
+       unique case (paddr)
+         REG_GLB_CTRL:  prdata <= {31'b0, ckg_bypass};
+         REG_IRPT_MSK:  prdata <= {30'b0, irpt_msk_q};
+         REG_IRPT_RAW:  prdata <= {30'b0, irpt_raw_q};
+         REG_IRPT_STAT: prdata <= {30'b0, (irpt_raw_q & ~irpt_msk_q)};
+         REG_IRPT_CLR:  prdata <= 32'b0;
+         REG_DBG_0:     prdata <= err_rd_addr_q;
+         REG_DBG_1:     prdata <= err_wr_addr_q;
+         REG_DBG_2:     prdata <= {16'b0, err_wr_id_q, err_rd_id_q};
+         REG_DBG_3:     prdata <= dbg_info_3;
+         default:       prdata <= 32'b0;
+       endcase
+    end
+    else begin
+       prdata <= prdata;
+    end
   end
 
 endmodule
