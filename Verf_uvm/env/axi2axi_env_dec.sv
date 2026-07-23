a78974916719ff1cb6268460a541705284ea12c6
yangshuo3672 <xys5793@gmail.com>
2026-07-23 10:41:48 +0800

    Update axi2axi_env_dec.sv

--- Verf_uvm/env/axi2axi_env_dec.sv
+++ Verf_uvm/env/axi2axi_env_dec.sv
@@ -1,11 +1,19 @@
 `idndef AXI2AXI_ENV_DEC__SV
 `define AXI2AXI_ENV_DEC__SV
 
+//declarations blk_a package 
+//Shared parameters and other declarations for blk_a block
 class axi2axi_env_dec extends linkbench_env_dec;
   
   parameter AXI_MST_NUM =1;
   parameter AXI_SLV_NUM =1;
   parameter APB_MST_NUM =1;
+
+  //Add parameter base on project requirment
+  //Coding begin
+  //%%%%%%%%%%%%%%%
+  //Coding end
+  
   
 endclass:axi2axi_env_dec
 
