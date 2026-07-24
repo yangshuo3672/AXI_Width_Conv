2a62a59b8f8b15e32fe8ac85999ed352d6a11ed8
yangshuo3672 <xys5793@gmail.com>
2026-07-24 17:31:09 +0800

    Create apb_driver_cfg.sv

---
+++ Verf_uvm/env/VIP_APB/src/apb_driver_cfg.sv
@@ -0,0 +1,89 @@
+`ifndef APB_DRIVER_CFG__SV
+`define APB_DRIVER_CFG__SV
+
+//apb interface agent driver configuration
+//This class contains the configuration for the apb interface agent driver
+
+class apb_driver_cfg extends uvm_object;
+
+  //Define the member variables base on project requirement
+  //-------------------------------Coding begin-------------------------------//
+  //%%%%%%%%%%%%%%%%%
+
+  bit rdata_x_statue_check_enable = 1'b1;
+  bit resp_check_enable           = 1'b1;
+  bit [1:0] do_idle_state         = 2'b0; //0: drive 0  //1: drive random //2: stable //3: NONE
+
+  int pready_cnt = 500;           //Add for jira833
+  int addr_width = 32 ;           //Add for jira1000
+  int data_width = 32 ;           //Add for jira1000
+  //-------------------------------Coding end-------------------------------//
+
+  `uvm_object_utils_begin(apb_driver_cfg)
+  //Add variables into field-automation base on project requirement
+  //-------------------------------Coding begin-------------------------------//
+  //%%%%%%%%%%%%%%%
+
+  `uvm_field_int(rdata_x_statue_check_enable, UVM_ALL_ON)
+  `uvm_field_int(resp_check_enable, UVM_ALL_ON)
+  `uvm_field_int(do_idle_state, UVM_ALL_ON)
+  `uvm_field_int(pready_cnt, UVM_ALL_ON)
+  `uvm_field_int(addr_width, UVM_ALL_ON)
+  `uvm_field_int(data_width, UVM_ALL_ON)
+  //-------------------------------Coding end-------------------------------//
+  `uvm_object_utils_end
+
+  //Constructor
+  extern function new (string name = "apb_driber_cfg" );
+
+  //pre_randomize.Handle random data information before randomize
+  extern function void pre_randomize();
+
+  //post_randomize.Handle random data information after randomize
+  extern function void post_randomize();
+
+  //Define the function/task base on project requirement
+  //-------------------------------Coding begin-------------------------------//
+  //%%%%%%%%%%%%%%%
+  //-------------------------------Coding end-------------------------------//
+
+endclass:apb_driver_cfg
+
+  //Define constraints of the variables base on project requirement
+  //-------------------------------Coding begin-------------------------------//
+  //%%%%%%%%%%%%%%%
+  //-------------------------------Coding end-------------------------------//
+
+function apb_driver_cfg::new(string name = "apb_driver_cfg");
+   super.new(name);
+   //Add the initial of variables base on project requirement
+   //-------------------------------Coding begin-------------------------------//
+   //%%%%%%%%%%%%%%%
+   //-------------------------------Coding end-------------------------------//
+endfunction: new
+
+function void apb_driver_cfg::pre_randomize;
+  super.pre_randomize();
+   //Extend pre_randomize() base on project requirement
+   //-------------------------------Coding begin-------------------------------//
+   //%%%%%%%%%%%%%%%
+   //-------------------------------Coding end-------------------------------//
+endfunction: pre_randomzie
+
+
+function void apb_driver_cfg::post_randomize;
+  super.post_randomize();
+   //Extend post_randomize() base on project requirement
+   //-------------------------------Coding begin-------------------------------//
+   //%%%%%%%%%%%%%%%
+   //-------------------------------Coding end-------------------------------//
+endfunction: post_randomzie
+
+
+`endif
+
+
+    
+
+
+  
