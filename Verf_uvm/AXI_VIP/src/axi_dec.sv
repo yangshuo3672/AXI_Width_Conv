2e34a3c251d4e8791c1e069c02908ef5fdce02c9
yangshuo3672 <xys5793@gmail.com>
2026-09-02 16:50:14 +0800

    Create axi_dec.sv

---
+++ Verf_uvm/AXI_VIP/src/axi_dec.sv
@@ -0,0 +1,7 @@
+typedef enum {DIR_READ = 0,DIR_WRITE = 1} axi_dir_enum;
+typedef enum {LENGTH_[1:256]} axi_length_enum
+typedef enum {
+    SIZE_8BIT = 0,
+    SIZE_16BIT = 1,
+    SIZE_32BIT = 2,
+}axi_xfer_seze_enum;
