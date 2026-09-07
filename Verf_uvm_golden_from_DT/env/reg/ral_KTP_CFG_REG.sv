1cdab331c46a0e9621d8e1d91fbcae3eb04046fc
yangshuo3672 <xys5793@gmail.com>
2026-09-07 11:56:09 +0800

    Update ral_KTP_CFG_REG.sv

--- Verf_uvm_golden_from_DT/env/reg/ral_KTP_CFG_REG.sv
+++ Verf_uvm_golden_from_DT/env/reg/ral_KTP_CFG_REG.sv
@@ -243,3 +243,167 @@ class ral_reg_KTP_CFG_REG_REG_KTP_DBG_INFO_3 extends uvm_reg;
 endclass : ral_reg_KTP_CFG_REG_REG_KTP_DBG_INFO_3
 
 
+//********************************************reg_block********************************************//
+class ral_block_KTP_CFG_REG extends uvm_reg_block;
+
+    rand ral_reg_KTP_CFG_REG_REG_KTP_GLB_CTRL REG_KTP_GLB_CTRL;
+    rand ral_reg_KTP_CFG_REG_REG_KTP_IRPT_MSK REG_KTP_IRPT_MSK;
+    rand ral_reg_KTP_CFG_REG_REG_KTP_IRPT_RAW REG_KTP_IRPT_RAW;
+    rand ral_reg_KTP_CFG_REG_REG_KTP_IRPT_STAT REG_KTP_IRPT_STAT;
+    rand ral_reg_KTP_CFG_REG_REG_KTP_IRPT_CLR REG_KTP_IRPT_CLR;
+    rand ral_reg_KTP_CFG_REG_REG_KTP_DBG_INFO_0 REG_KTP_DBG_INFO_0;
+    rand ral_reg_KTP_CFG_REG_REG_KTP_DBG_INFO_1 REG_KTP_DBG_INFO_1;
+    rand ral_reg_KTP_CFG_REG_REG_KTP_DBG_INFO_2 REG_KTP_DBG_INFO_2;
+    rand ral_reg_KTP_CFG_REG_REG_KTP_DBG_INFO_3 REG_KTP_DBG_INFO_3;
+
+    rand uvm_reg_field REG_KTP_GLB_CTRL_ckg_bypass;
+    uvm_reg_field ckg_bypass;
+    uvm_reg_field REG_KTP_GLB_CTRL_rsv_0;
+    rand uvm_reg_field REG_KTP_IRPT_MSK_ktp_rresp_err_irpt_msk;
+    rand uvm_reg_field ktp_rresp_err_irpt_msk;
+    rand uvm_reg_field REG_KTP_IRPT_MSK_ktp_wresp_err_irpt_msk;
+    rand uvm_reg_field ktp_wresp_err_irpt_msk;
+    uvm_reg_field REG_KTP_IRPT_MSK_rsv_0;
+    uvm_reg_field REG_KTP_IRPT_RAW_ktp_rresp_err_irpt_raw;
+    uvm_reg_field ktp_rresp_err_irpt_raw;
+    uvm_reg_field REG_KTP_IRPT_RAW_ktp_wresp_err_irpt_raw;
+    uvm_reg_field ktp_wresp_err_irpt_raw;
+    uvm_reg_field REG_KTP_IRPT_RAW_rsv_0;
+    uvm_reg_field REG_KTP_IRPT_STAT_ktp_rresp_err_irpt_stat;
+    uvm_reg_field ktp_rresp_err_irpt_stat;
+    uvm_reg_field REG_KTP_IRPT_STAT_ktp_wresp_err_irpt_stat;
+    uvm_reg_field ktp_wresp_err_irpt_stat;
+    uvm_reg_field REG_KTP_IRPT_STAT_rsv_0;
+    rand uvm_reg_field REG_KTP_IRPT_CLR_ktp_rresp_err_irpt_clr;
+    rand uvm_reg_field ktp_rresp_err_irpt_clr;
+    rand uvm_reg_field REG_KTP_IRPT_CLR_ktp_wresp_err_irpt_clr;
+    rand uvm_reg_field ktp_wresp_err_irpt_clr;
+    uvm_reg_field REG_KTP_IRPT_CLR_rsv_0;
+    uvm_reg_field REG_KTP_DBG_INFO_0_addr_for_error_read_command;
+    uvm_reg_field addr_for_error_read_command;
+    uvm_reg_field REG_KTP_DBG_INFO_1_addr_for_error_write_command;
+    uvm_reg_field addr_for_error_write_command;
+    uvm_reg_field REG_KTP_DBG_INFO_2_arid_for_error_read_command;
+    uvm_reg_field arid_for_error_read_command;
+    uvm_reg_field REG_KTP_DBG_INFO_2_awid_for_error_write_command;
+    uvm_reg_field awid_for_error_write_command;
+    uvm_reg_field REG_KTP_DBG_INFO_3_awready_for_upstream;
+    uvm_reg_field awready_for_upstream;
+    uvm_reg_field REG_KTP_DBG_INFO_3_arready_for_upstream;
+    uvm_reg_field arready_for_upstream;
+    uvm_reg_field REG_KTP_DBG_INFO_3_wready_for_upstream;
+    uvm_reg_field wready_for_upstream;
+    uvm_reg_field REG_KTP_DBG_INFO_3_rvalid_for_upstream;
+    uvm_reg_field rvalid_for_upstream;
+    uvm_reg_field REG_KTP_DBG_INFO_3_bvalid_for_upstream;
+    uvm_reg_field bvalid_for_upstream;
+   uvm_reg_field REG_KTP_DBG_INFO_3_awvalid_for_downstream;
+   uvm_reg_field awvalid_for_downstream;
+   uvm_reg_field REG_KTP_DBG_INFO_3_arvalid_for_downstream;
+   uvm_reg_field arvalid_for_downstream;
+   uvm_reg_field REG_KTP_DBG_INFO_3_wvalid_for_downstream;
+   uvm_reg_field wvalid_for_downstream;
+   uvm_reg_field REG_KTP_DBG_INFO_3_rready_for_downstream;
+   uvm_reg_field rready_for_downstream;
+   uvm_reg_field REG_KTP_DBG_INFO_3_bready_for_downstream;
+   uvm_reg_field bready_for_downstream;
+   uvm_reg_field REG_KTP_DBG_INFO_3_awwvalid_for_axi2uif;
+   uvm_reg_field awwvalid_for_axi2uif;
+   uvm_reg_field REG_KTP_DBG_INFO_3_awwready_for_axi2uif;
+   uvm_reg_field awwready_for_axi2uif;
+   uvm_reg_field REG_KTP_DBG_INFO_3_afifo_full_for_aww_master_side;
+   uvm_reg_field afifo_full_for_aww_master_side;
+   uvm_reg_field REG_KTP_DBG_INFO_3_afifo_empty_for_aww_slave_side;
+   uvm_reg_field afifo_empty_for_aww_slave_side;
+   uvm_reg_field REG_KTP_DBG_INFO_3_afifo_full_for_ar_master_side;
+   uvm_reg_field afifo_full_for_ar_master_side;
+   uvm_reg_field REG_KTP_DBG_INFO_3_afifo_empty_for_ar_slave_side;
+   uvm_reg_field afifo_empty_for_ar_slave_side;
+   uvm_reg_field REG_KTP_DBG_INFO_3_afifo_full_for_b_slave_side;
+   uvm_reg_field afifo_full_for_b_slave_side;
+   uvm_reg_field REG_KTP_DBG_INFO_3_afifo_empty_for_b_master_side;
+   uvm_reg_field afifo_empty_for_b_master_side;
+   uvm_reg_field REG_KTP_DBG_INFO_3_afifo_full_for_r_slave_side;
+   uvm_reg_field afifo_full_for_r_slave_side;
+   uvm_reg_field REG_KTP_DBG_INFO_3_afifo_empty_for_r_master_side;
+   uvm_reg_field afifo_empty_for_r_master_side;
+   uvm_reg_field REG_KTP_DBG_INFO_3_awwvalid_for_uif2axi;
+   uvm_reg_field awwvalid_for_uif2axi;
+   uvm_reg_field REG_KTP_DBG_INFO_3_awwready_for_uif2axi;
+   uvm_reg_field awwready_for_uif2axi;
+   uvm_reg_field REG_KTP_DBG_INFO_3_rsv_0;
+
+   function new(string name = "KTP_CFG_REG");
+       super.new(name, build_coverage(UVM_NO_COVERAGE));
+   endfunction: new
+
+virtual function void build();
+    this.default_map = create_map("", 0, 4, UVM_LITTLE_ENDIAN, 0);
+    this.REG_KTP_GLB_CTRL = ral_reg_KTP_CFG_REG_REG_KTP_GLB_CTRL::type_id::create("REG_KTP_GLB_CTRL",,get_full_name());
+    this.REG_KTP_GLB_CTRL.configure(this, null, "");
+    this.REG_KTP_GLB_CTRL.build();
+        this.REG_KTP_GLB_CTRL.add_hdl_path('{
+            '{"HDL_REG_KTP_GLB_CTRL", -1, -1}
+        });
+    this.default_map.add_reg(this.REG_KTP_GLB_CTRL, `UVM_REG_ADDR_WIDTH'h0, "RW", 0);
+        this.REG_KTP_GLB_CTRL_ckg_bypass = this.REG_KTP_GLB_CTRL.ckg_bypass;
+        this.ckg_bypass = this.REG_KTP_GLB_CTRL.ckg_bypass;
+        this.REG_KTP_GLB_CTRL_rsv_0 = this.REG_KTP_GLB_CTRL.rsv_0;
+    this.REG_KTP_IRPT_MSK = ral_reg_KTP_CFG_REG_REG_KTP_IRPT_MSK::type_id::create("REG_KTP_IRPT_MSK",,get_full_name());
+    this.REG_KTP_IRPT_MSK.configure(this, null, "");
+    this.REG_KTP_IRPT_MSK.build();
+        this.REG_KTP_IRPT_MSK.add_hdl_path('{
+            '{"HDL_REG_KTP_IRPT_MSK", -1, -1}
+        });
+    this.default_map.add_reg(this.REG_KTP_IRPT_MSK, `UVM_REG_ADDR_WIDTH'h10, "RW", 0);
+    this.REG_KTP_IRPT_MSK_ktp_rresp_err_irpt_msk = this.REG_KTP_IRPT_MSK.ktp_rresp_err_irpt_msk;
+    this.ktp_rresp_err_irpt_msk = this.REG_KTP_IRPT_MSK.ktp_rresp_err_irpt_msk;
+    this.REG_KTP_IRPT_MSK_ktp_wresp_err_irpt_msk = this.REG_KTP_IRPT_MSK.ktp_wresp_err_irpt_msk;
+    this.ktp_wresp_err_irpt_msk = this.REG_KTP_IRPT_MSK.ktp_wresp_err_irpt_msk;
+    this.REG_KTP_IRPT_MSK_rsv_0 = this.REG_KTP_IRPT_MSK.rsv_0;
+    this.REG_KTP_IRPT_RAW = ral_reg_KTP_CFG_REG_REG_KTP_IRPT_RAW::type_id::create("REG_KTP_IRPT_RAW",,get_full_name());
+    this.REG_KTP_IRPT_RAW.configure(this, null, "");
+    this.REG_KTP_IRPT_RAW.build();
+    this.default_map.add_reg(this.REG_KTP_IRPT_RAW, `UVM_REG_ADDR_WIDTH'h14, "RO", 0);
+        this.REG_KTP_IRPT_RAW_ktp_rresp_err_irpt_raw = this.REG_KTP_IRPT_RAW.ktp_rresp_err_irpt_raw;
+        this.ktp_rresp_err_irpt_raw = this.REG_KTP_IRPT_RAW.ktp_rresp_err_irpt_raw;
+        this.REG_KTP_IRPT_RAW_ktp_wresp_err_irpt_raw = this.REG_KTP_IRPT_RAW.ktp_wresp_err_irpt_raw;
+        this.ktp_wresp_err_irpt_raw = this.REG_KTP_IRPT_RAW.ktp_wresp_err_irpt_raw;
+        this.REG_KTP_IRPT_RAW_rsv_0 = this.REG_KTP_IRPT_RAW.rsv_0;
+    this.REG_KTP_IRPT_STAT = ral_reg_KTP_CFG_REG_REG_KTP_IRPT_STAT::type_id::create("REG_KTP_IRPT_STAT",,get_full_name());
+    this.REG_KTP_IRPT_STAT.configure(this, null, "");
+    this.REG_KTP_IRPT_STAT.build();
+    this.default_map.add_reg(this.REG_KTP_IRPT_STAT, `UVM_REG_ADDR_WIDTH'h18, "RO", 0);
+        this.REG_KTP_IRPT_STAT_ktp_rresp_err_irpt_stat = this.REG_KTP_IRPT_STAT.ktp_rresp_err_irpt_stat;
+        this.ktp_rresp_err_irpt_stat = this.REG_KTP_IRPT_STAT.ktp_rresp_err_irpt_stat;
+        this.REG_KTP_IRPT_STAT_ktp_wresp_err_irpt_stat = this.REG_KTP_IRPT_STAT.ktp_wresp_err_irpt_stat;
+        this.ktp_wresp_err_irpt_stat = this.REG_KTP_IRPT_STAT.ktp_wresp_err_irpt_stat;
+        this.REG_KTP_IRPT_STAT_rsv_0 = this.REG_KTP_IRPT_STAT.rsv_0;
+    this.REG_KTP_IRPT_CLR = ral_reg_KTP_CFG_REG_REG_KTP_IRPT_CLR::type_id::create("REG_KTP_IRPT_CLR",,get_full_name());
+    this.REG_KTP_IRPT_CLR.configure(this, null, "");
+    this.REG_KTP_IRPT_CLR.build();
+    this.default_map.add_reg(this.REG_KTP_IRPT_CLR, `UVM_REG_ADDR_WIDTH'h1c, "RW", 0);
+        this.REG_KTP_IRPT_CLR_ktp_rresp_err_irpt_clr = this.REG_KTP_IRPT_CLR.ktp_rresp_err_irpt_clr;
+        this.ktp_rresp_err_irpt_clr = this.REG_KTP_IRPT_CLR.ktp_rresp_err_irpt_clr;
+        this.REG_KTP_IRPT_CLR_ktp_wresp_err_irpt_clr = this.REG_KTP_IRPT_CLR.ktp_wresp_err_irpt_clr;
+        this.ktp_wresp_err_irpt_clr = this.REG_KTP_IRPT_CLR.ktp_wresp_err_irpt_clr;
+        this.REG_KTP_IRPT_CLR_rsv_0 = this.REG_KTP_IRPT_CLR.rsv_0;
+
+    //中间是其他寄存器，不一一列举了
+
+    //setting up backdoor access...
+    begin
+        ral_reg_KTP_CFG_REG_REG_KTP_GLB_CTRL_bkdr bkdr = new(this.REG_KTP_GLB_CTRL.get_full_name());
+        this.REG_KTP_GLB_CTRL.set_backdoor(bkdr);
+    end
+    begin
+        ral_reg_KTP_CFG_REG_REG_KTP_IRPT_MSK_bkdr bkdr = new(this.REG_KTP_IRPT_MSK.get_full_name());
+        this.REG_KTP_IRPT_MSK.set_backdoor(bkdr);
+    end
+endfunction : build
+
+    `uvm_object_utils(ral_block_KTP_CFG_REG)
+
+endclass : ral_block_KTP_CFG_REG
+
+
